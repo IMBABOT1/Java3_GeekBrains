e6ad13c51843d32082e1f2e6f143dea069129910
unknown <zlovred12@gmail.com>
2022-06-16 20:03:41 +0300

    temp

--- src/main/java/ru/geekbrains/java3/Lesson1/Fruits/Main.java
+++ src/main/java/ru/geekbrains/java3/Lesson1/Fruits/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
         orangeBox.pour(orangeBox1);
 
-      / System.out.println(orangeBox1);
+      // System.out.println(orangeBox1);
 
         System.out.println(orangeBox);
 
